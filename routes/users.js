a2d86f96dc8c09a1bcdb2a8efac709e56979ffb9
Blake Davies-Long <blake.davies.long@gmail.com>
2024-06-13 16:37:50 -0400

    removed old code

--- routes/users.js
+++ routes/users.js
@@ -18,33 +18,3 @@ usersRouter.get('/:id', async (req, res, next) => {
         res.status(500).send('Internal Server Error');
     }
 });
-
-usersRouter.get('/:id/cart', async (req, res, next) => {
-    try{
-        const user = await db.User.findOne({ where: { id: req.params.id } });
-        if (!user) {
-            console.log('User not found');
-            return res.status(404).send('User not found');
-        }
-
-        let cart = await db.Cart.findOne({ where: { user_id: user.id } });
-        if (!cart) {
-            cart = await db.Cart.create({ user_id: user.id });
-        }
-
-        const cartItems = await db.CartItem.findAll({ where: { cart_id: cart.id } });
-        const productIds = cartItems.map(item => item.product_id);
-        const products = await db.Product.findAll({ where: { id: productIds } });
-
-        const cartItemsWithProducts = cartItems.map(item => {
-            const product = products.find(prod => prod.id === item.product_id);
-            return { cartItem: item, product };
-        });
-        console.log(cartItemsWithProducts);
-        res.render('cart', { user, cartItems: cartItemsWithProducts });
-
-    } catch (error) {
-        console.error('Error fetching user profile:', error);
-        res.status(500).send('Internal Server Error');
-    }
-});
